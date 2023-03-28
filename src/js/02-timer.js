c9f0b8d38a5484fd10180fefe0012d799332db78
keastar <keastar1986@gmail.com>
2023-03-28 14:49:41 +0300

    link parcel
    link parcel

---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+const flatpickr = require("flatpickr");
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+// all modules
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
---
+++ src/js/03-promises.js
@@ -0,0 +1,19 @@
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
+
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
