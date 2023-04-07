f5e62ee556fd332bb18fa6f0f12af892e44e6fe0
keastar <keastar1986@gmail.com>
2023-04-07 14:19:31 +0300

    3task_theory_of_promises
    3task_theory_of_promises

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+//импортируем библеотеку разворачивающегося календаря
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 // all modules
 // import Notiflix from 'notiflix';
@@ -11,67 +12,79 @@ import "flatpickr/dist/flatpickr.min.css";
 // import { Loading } from 'notiflix/build/notiflix-loading-aio';
 // import { Block } from 'notiflix/build/notiflix-block-aio';
 
+//объявляем переменные для JS из HTML
 const refs = {
-    dateInputEl: document.querySelector('#datetime-picker'),
-    startBtn: document.querySelector('button[data-start]'),
-    clockFace: document.querySelector('.timer'),
+  dateInputEl: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  clockFace: document.querySelector('.timer'),
 };
 
 const data = {
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-    seconds: document.querySelector('[data-seconds]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
-
+//обьявляем интервал
 let IntervalId;
-
-
+//обьявляем обьект со свойствами и ф-цией selectedDates
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    altInput: true,
-    altFormat: 'd M Y H:i',
-    dateFormat: 'Y-m-d H:i',
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose: (selectedDates) => {
-        console.log(selectedDates);
-         if (selectedDates[0] < new Date()) {
-             window.alert('please choose another date in the future');
-             refs.startBtn.addAttribute('disabled');
-         } else {
-             refs.startBtn.removeAttribute('disabled');
-
-        }
-    },
+  enableTime: true,
+  time_24hr: true,
+  altInput: true,
+  altFormat: 'd M Y H:i',
+  dateFormat: 'Y-m-d H:i',
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose: selectedDates => {
+    console.log(selectedDates);
+    //если первая выбранная дата раньше текущей даты, то предупреждение - по выбору даты из будущего
+    // и кнопка старт - неактивна
+    if (selectedDates[0] < new Date()) {
+      window.alert('please choose another date in the future');
+      refs.startBtn.addAttribute('disabled');
+      // в другом случае кнопка старт активна
+    } else {
+      refs.startBtn.removeAttribute('disabled');
+    }
+  },
 };
 
-
-refs.startBtn.addEventListener('click', () => {
-    const targetTime = fp.selectedDates[0];
-    clearInterval(IntervalId);
-                 IntervalId = setInterval(() => {
-                     const deltaTime =  targetTime - new Date();
-                     const { days, hours, minutes, seconds } = convertMs(deltaTime);
-                     // Update the timer interface
-                     data.days.textContent = days;
-                     data.hours.textContent = hours;
-                     data.minutes.textContent = minutes;
-                     data.seconds.textContent = seconds;
-                     if (deltaTime <= 1000) {
-                         clearInterval(IntervalId);
-                     }
-                 }, 1000);
-                 
-             });
-
+//ф-ция flatpickr принимает 2ва элемента(элемент, на который необходимо повесить событие, чтобы показывать календарь;
+// опции к этому календарю, которые я буду использовать)
+// все addEventListener, ф-ции обработчики, конвертации дат - это все внутри библиотеки flatpickr
 const fp = flatpickr(refs.dateInputEl, options);
 // console.log(fp);
 
+//по нажатию на кнопку старт запускается ф-ция подсчета дельта времени через каждую секунду
+// и каждую секунду происходит обновление таймера интерфейса по определенным значениям
+// days, hours, minutes, seconds;
+refs.startBtn.addEventListener('click', () => {
+  //после нажатия на кнопку start интервал должен поменятся:
+  //очистится предыдущий интервал и запуститься новый
+  const targetTime = fp.selectedDates[0];
+  clearInterval(IntervalId);
+  IntervalId = setInterval(() => {
+    const deltaTime = targetTime - new Date();
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    // Update the timer interface
+    data.days.textContent = days;
+    data.hours.textContent = hours;
+    data.minutes.textContent = minutes;
+    data.seconds.textContent = seconds;
+    if (deltaTime <= 1000) {
+      clearInterval(IntervalId);
+    }
+  }, 1000);
+});
+
+//ф-ция добавления "0" перед элементами счетчика
 function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
+  return String(value).padStart(2, '0');
 }
+//ф-ция принимает кол-во млсек, пройденное с 01.01.1970
+// и возвращает объект, в котором содержатся поля
+// days, hours, minutes и seconds, которые конвертированные из этих млсек
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -87,7 +100,9 @@ function convertMs(ms) {
   // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
@@ -95,4 +110,3 @@ function convertMs(ms) {
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,13 +7,67 @@ import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Block } from 'notiflix/build/notiflix-block-aio';
 
+// const refs = {
+//   delayInputEl: document.querySelector('#datetime-picker'),
+//   stepMsInputEl: document.querySelector('button[data-start]'),
+//   amountInputEl: document.querySelector('.timer'),
+// };
 
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//     resolve('Промис выполнился успешно с результатом Fulfilled');
+//   } else {
+//     // Reject
+//     reject('Промис выпонился с ошибкой и был откланен Rejected');
+//   }
+// }
+// console.log(createPromise);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.3;
+  setTimeout(() => {
+    if (canFulfill) {
+      // Fulfill
+      resolve('Промис выполнился успешно с результатом Fulfilled');
+    } else {
+      // Reject
+      reject('Промис выпонился с ошибкой и был откланен Rejected');
+    }
+  }, 2000);
+});
+// console.log(promise);
+//promise выполнился успешно после 2сек загрузки экрана, т.к. код асинхронный
+//результат выполнения промиса вернется и загрузится после 2 сек, т.к.используем setTimeout
+// promise.then(
+//   //метод .then можно передать 2ве ф-ции  .then(on Success, onError);
+//   //ф-ция возвращает через 2ве сек результат resolve в виде текста
+
+//   result => {
+//     // результат resolve
+//     // console.log(result);
+//     console.log(`${result}`);
+//   },
+//   //ф-ция возвращает через 2ве сек результат reject в виде текста
+//   error => {
+//     // результат reject
+//     // console.log(error);
+//     console.log(`${error}`);
+//   }
+// );
+//цепочки промисов: result - возвращает успешный resolve
+// и результат return 5 передается из 1го then, как промис(успеха) и 2й then возвращает х=5 => 5;
+// и результат return 10 передается из 2го then, как промис(успеха) и 3й then возвращает y=10 => 10;
+promise
+  .then(result => {
+    console.log(result);
+    return 5;
+  })
+  .then(x => {
+    console.log(x);
+    return 10;
+  })
+  .then(y => {
+    console.log(y);
+  });
